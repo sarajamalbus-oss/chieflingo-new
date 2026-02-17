58dd049c632a1f9d8fd2bdf7d93c2da1f3f5bb59
Sara <sarajamalbus.gmail.com>
2026-02-17 10:55:15 -0800

    fix: stripe build error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY!,
+  process.env.STRIPE_SECRET_KEY ?? "dummy_key_for_build",
   {
     apiVersion: "2026-01-28.clover",
     typescript: true,
