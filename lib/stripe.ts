e91103acf2ad8fd757615b236b6f3f01899c6420
Sara <sarajamalbus.gmail.com>
2026-05-04 21:49:38 +0400

    fix stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY ?? "dummy_key_for_build",
   {
-    apiVersion: "2026-01-28.clover",
+    apiVersion: "2026-02-25.clover",
     typescript: true,
   }
 );
